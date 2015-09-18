f53e75fd344e0426fe4ca4956cd651fe73f5aa30
Cauffe <cauffe@me.com>
2015-09-18 20:33:49 +0300

    signup views

--- main/forms.py
+++ main/forms.py
@@ -3,12 +3,10 @@ from django.core.validators import RegexValidator
 
 from main.models import Company
 
-letter_validator = RegexValidator(r'^[a-zA-Z]*$', 'Please Type Letters')
-
 
 class UserSignUp(forms.Form):
-    name = forms.CharField(required=True, validators=[letter_validator])
-    password = forms.CharField(required=True)
+    name = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
     email = forms.CharField(required=True)
 
 
--- main/views.py
+++ main/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from main.models import Company, Tender, Quotation
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from main.forms import UserSignUp, UserLogin, CreateTender, CreateTender, CreateItem, EditTender, EditQuote, EditItem
-
+from django.contrib.auth import authenticate, login, logout
+from django.template import RequestContext
 
 # Create your views here.
+
+def signup(request):
+    context = {}
+
+    form = UserSignUp()
+
+    context['form'] = form
+
+    if request.method == 'POST':
+        form = UserSignUp(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            email = None
+
+            new_user = User.objects.create_user(username, email, password)
+
+            auth_user = authenticate(username=username, password=password)
+
+
+            login(request, auth_user)
+
+            return HttpResponseRedirect('/list_view/')
+
+    return render_to_response('signup.html', context, context_instance=RequestContext(request))
 
 
 def home(request):
